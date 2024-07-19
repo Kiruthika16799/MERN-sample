175ed0889ff2715e0479c9666306ad936c731746
Kiruthika S <62058246+Kiruthika16799@users.noreply.github.com>
2024-07-19 13:17:12 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 const PORT = 5000;
 
-const mongoDBURL = 'mongodb+srv://root:root@book-store-mern.rekk2nb.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN';
+const mongoDBURL = '######'; // MongoDB connection 
 
 module.exports = { PORT, mongoDBURL }
